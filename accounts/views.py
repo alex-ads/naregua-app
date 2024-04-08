8a2be6ce9bbdcb6c69c5b3dffbc48c79060dab8c
Alex <alex.silva.data@gmail.com>
2024-04-08 17:37:10 -0300

    ajustes

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .admin import CustomUserCreationForm
 from django.contrib import messages
+from django.db.utils import IntegrityError
 
 
 # Create your views here.
@@ -12,12 +13,19 @@ def register(request):
         form = CustomUserCreationForm(request.POST)
 
         if form.is_valid():
-            user = form.save(commit=False)
-            user.is_active = False  # Define o usuário como desativado
-            user.save()
-            messages.success(
-                request, "Registrado com Sucesso! Entre em contato para ativar conta.")
-            form = CustomUserCreationForm()
+            try:
+                user = form.save(commit=False)
+                user.is_active = False  # Define o usuário como desativado
+                user.save()
+                messages.success(
+                    request, "Registrado com Sucesso! Entre em contato para ativar conta.")
+                form = CustomUserCreationForm()
+            except IntegrityError as e:
+                # Tratar erros de integridade, como tentativa de registro com email duplicado
+                messages.error(request, f"Erro ao registrar: {e}")
+            except Exception as e:
+                # Lidar com outras exceções não esperadas
+                messages.error(request, f"Erro inesperado ao registrar: {e}")
         else:
             for field, errors in form.errors.items():
                 for error in errors:
