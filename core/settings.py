42a30d895145cccc1bc68e816ad18e63545f33bb
Alex <alex.silva.data@gmail.com>
2024-04-27 20:43:50 -0300

    ajustes config

--- core/settings.py
+++ core/settings.py
@@ -37,10 +37,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG') == 'True'
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
 
 
 # Application definition
